cdc8c6233a6f15909aad64aec3759452484afb7e
Járosi István Zsolt <76613843+SteveJarosi@users.noreply.github.com>
2021-02-02 10:18:03 +0200

    Add files via upload

--- xchange.js
+++ xchange.js
@@ -1,42 +1,41 @@
-let rList ={};
+let rList = {};
 
-const getRates= async ()=>{
-    const url= 
-    `https://api.exchangeratesapi.io/latest?base=USD`;
-try{
-    const response = await fetch(url);
-    const data = await response.json();
-    let {rates}=data;
-    for(let currencyCode in rates){
-        rList[currencyCode] = rates[currencyCode];
+const getRates = async () => {
+    const url =
+        `https://api.exchangeratesapi.io/latest?base=USD`;
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        let { rates } = data;
+        for (let currencyCode in rates) {
+            rList[currencyCode] = rates[currencyCode];
             addRow("dataTable", currencyCode);
-    }
-}catch(e){
-    console.log("Oops! error: ",e);
+        }
+    } catch (e) {
+        console.log("Oops! error: ", e);
     }
 }
 
 function rCalc() {
-    for(let i = 1; i < document.getElementById("dataTable").rows.length; i++){
+    for (let i = 1; i < document.getElementById("dataTable").rows.length; i++) {
         if (Number(document.getElementById(`rw${i}`).value) !== 0) {
-            let vin = (document.getElementById("dataTable").rows[i].cells[0].innerHTML) / 
-            Number(document.getElementById(`rw${i}`).value);
-               for (let k = 1; k < document.getElementById("dataTable").rows.length; k++)
-               {
-                document.getElementById(`rw${k}`).placeholder = 
-                ((document.getElementById("dataTable").rows[k].cells[0].innerHTML) / vin).toFixed(2);
-                }
+            let vin = (document.getElementById("dataTable").rows[i].cells[0].innerHTML) /
+                Number(document.getElementById(`rw${i}`).value);
+            for (let k = 1; k < document.getElementById("dataTable").rows.length; k++) {
+                document.getElementById(`rw${k}`).placeholder =
+                    ((document.getElementById("dataTable").rows[k].cells[0].innerHTML) / vin).toFixed(2);
             }
-        }    
+        }
     }
+}
 
 
 function rClear() {
-    for(i = 1; i < document.getElementById("dataTable").rows.length; i++){
+    for (i = 1; i < document.getElementById("dataTable").rows.length; i++) {
         document.getElementById(`rw${i}`).value = "";
         document.getElementById(`rw${i}`).placeholder = "";
-        }
     }
+}
 
 function addRow(tableID, currencyCode) {
 
@@ -52,9 +51,9 @@ function addRow(tableID, currencyCode) {
     cell4.appendChild(element4);
     element4.type = "number";
     element4.min = 0;
-    element4.setAttribute("id",`rw${rowCount}`);
+    element4.setAttribute("id", `rw${rowCount}`);
     element4.addEventListener('input', rCalc);
     element4.addEventListener('focus', rClear);
-    }
+}
 
 getRates();
